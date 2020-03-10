4ed063b660f0902d51f9397a688e17fa7a110a79
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-03-10 18:49:37 +0700

    siamese fix

--- pytorch_model/siamese.py
+++ pytorch_model/siamese.py
@@ -1,7 +1,7 @@
 import os
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 import torchvision
 import torchvision.datasets as dset

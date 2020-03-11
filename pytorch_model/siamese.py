a1224ee3a7723e0c84eb2d94bbf71dbbfe370cd0
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-03-11 18:30:09 +0700

    fix siamese loss

--- pytorch_model/siamese.py
+++ pytorch_model/siamese.py
@@ -116,7 +116,7 @@ class ContrastiveLoss(torch.nn.Module):
         # loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
         #                               (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
         loss_contrastive = torch.mean((label) * torch.pow(euclidean_distance, 2) +
-                                      (1-label) * torch.max(torch.tensor(0.0).to(device),torch.pow(torch.tensor(self.margin).to(device) - euclidean_distance, 2)))
+                                      (1-label) * torch.max(torch.tensor(0.0).to(self.device),torch.pow(torch.tensor(self.margin).to(self.device) - euclidean_distance, 2)))
         return loss_contrastive
 
 if __name__ == "__main__":

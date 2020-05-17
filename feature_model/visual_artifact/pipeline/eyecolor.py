80077d838a46d45d08e1689375df0b6d1136c560
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-05-17 22:07:34 +0700

    add model use feature, add adjust constrast, brightness, Add siamese efficient

--- feature_model/visual_artifact/pipeline/eyecolor.py
+++ feature_model/visual_artifact/pipeline/eyecolor.py
@@ -8,8 +8,8 @@ from skimage.transform import hough_circle, hough_circle_peaks
 from skimage.feature import canny
 from skimage import exposure
 
-import pipeline_utils
-import face_utils
+import feature_model.visual_artifact.pipeline_utils
+import feature_model.visual_artifact.face_utils
 
 
 def extract_reflection(img, mask):

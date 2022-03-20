d9b6d5acabe9565ed2b1476be216069a062eb9f1
MInh Tam <pminhtamnb@gmail.com>
2022-03-20 15:52:03 +0700

    move image val from train

--- preprocess_data/move_images.py
+++ preprocess_data/move_images.py
@@ -1,5 +1,25 @@
 import glob
 import os
 import shutil
+import random
+from tqdm import tqdm
 
-# list_image =
+random.seed(0)
+train_folder = "/home/tampm/df_in_the_wild/image_jpg/train/"
+list_image_1_df = glob.glob("/home/tampm/df_in_the_wild/image_jpg/train/1_df/*")
+list_image_0_real = glob.glob("/home/tampm/df_in_the_wild/image_jpg/train/0_real/*")
+val_folder = train_folder.replace("/train/","/val/")
+random.shuffle(list_image_1_df)
+random.shuffle(list_image_0_real)
+
+if not os.path.exists(val_folder):
+    os.makedirs(val_folder)
+if not os.path.exists(val_folder+"/1_df/"):
+    os.makedirs(val_folder+"/1_df/")
+if not os.path.exists(val_folder+"/0_real/"):
+    os.makedirs(val_folder+"/0_real/")
+for i in tqdm(range(50000)):
+    df_image = list_image_1_df.pop()
+    real_image = list_image_0_real.pop()
+    shutil.move(df_image,val_folder+"/1_df/")
+    shutil.move(real_image,val_folder+"/0_real/")
